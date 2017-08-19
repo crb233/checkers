0ccb6bd2cb75c64f3e96b0e4245a788d7e1b6f53
hk476@drexel.edu <hk476@drexel.edu>
2017-08-19 17:26:28 -0400

    fix  UI

--- src/public/javascript/clientManager.js
+++ src/public/javascript/clientManager.js
@@ -80,6 +80,7 @@
 	function joinGame(){
 		document.getElementById("newGameForm").style.display = "none";
 		document.getElementById("games").style.display = "block";
+		document.getElementById("games").innerHTML = "";
 		document.getElementById("joinGameForm").style.display = "block";
 		
 		
