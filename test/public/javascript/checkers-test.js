76a996969df758763ddff559eb48cfda278c9dc5
Samuel Eliot Nathanson <sen52@drexel.edu>
2017-08-21 18:29:58 -0400

    Added sample test for checkers-test.js

---
+++ test/public/javascript/checkers-test.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+
+describe("checkers.js", function() {
+
+  describe("makeMove()", function() {
+
+    it("should do ___ when we ___", function() {
+      assert(true);
+    });
+
+  });
+
+});
